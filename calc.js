599897e9c2bc4394671a9d24cba8339f2ea1d9d1
Carina C <cardozo1747@gmail.com>
2022-12-18 23:44:28 -0600

    cacl

--- calc.js
+++ calc.js
@@ -18,3 +18,33 @@ const decimal = document.querySelector(".decimal");
 decimal.addEventListener("click", () => {
   addDecimal();
 });
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", clearCalculator);
+
+const numberButtons = document.querySelectorAll(".num");
+
+const operators = document.querySelectorAll(".operator");
+
+numberButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    handleNumber(e.target.textContent);
+  });
+});
+
+function handleNumber(number) {
+  if (previousNum !== "" && currentNum !== "" && operator === "") {
+    previousNum = "";
+    currentDisplayNumber.textContent = currentNum;
+  }
+  if (currentNum.length <= 11) {
+    currentNum += number;
+    currentDisplayNumber.textContent = currentNum;
+  }
+}
+
+operators.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    handleOperator(e.target.textContent);
+  });
+});
